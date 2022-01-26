91911bc648053a08813b5bc817efa837260d8146
Sofya <vampiresszero@gmail.com>
2022-01-26 17:20:45 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) { //Вывод всей страницы
+    const [edit, setEdit] = useState(false);
+    function EditPage() {
+        return (
+            <div>
+                <h1>Редактирование страницы</h1>
+                <div className="blockEdit">
+                    <span>Название</span>
+                    <input type="text" name="name" id="" />
+                    <span>Ссылка</span>
+                    <input type="text" name="link" id="" />
+                    <span className="spanText">Текст</span>
+                    <textarea name="text" id="">qwertytuyi</textarea>
+                </div>
+                <button>Сохранить</button>
+            </div>
+        )
+    }
+    function Section(props) { //Вывод блока с заглавием
+        let chapter;
+        if (props.data.chapter !== null) {
+            chapter = <div className="content">{props.data.chapter.map(function (item, i) { return (<Chapter key={i} data={item} sectionLink={props.data.link} />) })}</div>;
+        } else {
+        }
+        function editChapter() {
+            setEdit(true);
+        }
+        return (
+            <div className="block">
+                <div className="nameSection"><a onClick={editChapter} href={props.data.link}>{props.data.section}</a></div>
+                {chapter}
+            </div>
+        );
+    }
+    function Chapter(props) { //Вывод элементов блока
+        return (<div className="chapter"><a href={props.sectionLink + props.data.link}>{props.data.name}</a></div>)
+    }
+    return (
+        <div>
+            <h1>Страницы сайта</h1>
+            <div className="page">
+                {props.data.map(function (item, i) { return (<Section key={i} data={item} />) })}
+            </div>
+        </div>
+    )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,70 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// let data = [
+//     { section: "Новости", link: "/", chapter: null },
+//     { section: "О нас", link: "/about/", chapter: [{ name: "Правоустанавливающие документы", link: "docs/" }, { name: "Общие сведения", link: "general/" }, { name: "Реквизиты УК", link: "props/" }] },
+//     { section: "Жителям", link: "/citizen/", chapter: [{ name: "Правовая база", link: "base/" }, { name: "Образцы заявлений", link: "example/" }, { name: "Паспортный стол", link: "passport/" }, { name: "Оплата услуг ЖКХ", link: "pay/" }, { name: "Отчет о проделанной работе", link: "reports" }] }
+// ];
+
+// function Page(props) { //Вывод всей страницы
+//     return (
+//         <div>
+//             <h1>Страницы сайта</h1>
+//             <div className="page">
+//                 {props.data.map(function (item, i) { return (<Section key={i} data={item} />) })}
+//             </div>
+//         </div>
+//     )
+// }
+// function EditPage() {
+//     return (
+//         <div>
+//             <h1>Редактирование страницы</h1>
+//             <div className="blockEdit">
+//                 <span>Название</span>
+//                 <input type="text" name="name" id="" />
+//                 <span>Ссылка</span>
+//                 <input type="text" name="link" id="" />
+//                 <span className="spanText">Текст</span>
+//                 <textarea name="text" id="">qwertytuyi</textarea>
+//             </div>
+//             <button>Сохранить</button>
+//         </div>
+//     )
+// }
+
+// function Section(props) { //Вывод блока с заглавием
+//     let chapter;
+//     if (props.data.chapter !== null) {
+//         chapter = <div className="content">{props.data.chapter.map(function (item, i) { return (<Chapter key={i} data={item} sectionLink={props.data.link} />) })}</div>;
+//     } else {
+//     }
+//     function edit(){
+//         alert("jknjk")
+//     }
+//     return (
+//         <div className="block">
+//             <div className="nameSection"><a onClick={edit} href={props.data.link}>{props.data.section}</a></div>
+//             {chapter}
+//         </div>
+//     );
+// }
+// function Chapter(props) { //Вывод элементов блока
+//     return (<div className="chapter"><a href={props.sectionLink + props.data.link}>{props.data.name}</a></div>)
+// }
+let data = [
+    { section: "Новости", link: "/", chapter: null },
+    { section: "О нас", link: "/about/", chapter: [{ name: "Правоустанавливающие документы", link: "docs/" }, { name: "Общие сведения", link: "general/" }, { name: "Реквизиты УК", link: "props/" }] },
+    { section: "Жителям", link: "/citizen/", chapter: [{ name: "Правовая база", link: "base/" }, { name: "Образцы заявлений", link: "example/" }, { name: "Паспортный стол", link: "passport/" }, { name: "Оплата услуг ЖКХ", link: "pay/" }, { name: "Отчет о проделанной работе", link: "reports" }] }
+];
+ReactDOM.render(<App data={data} />, document.getElementById("root"));
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
